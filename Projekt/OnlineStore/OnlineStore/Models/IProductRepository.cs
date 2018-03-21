0ff3133344b95a963a3f671551fc188c5d6038ff
Marek Hermansa <marekhermansa@gmail.com>
2018-03-21 13:27:12 +0100

    restore project

---
+++ Projekt/OnlineStore/OnlineStore/Models/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿// logic for storing and retrieving the data from 
+// the persistent data store
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Models
+{
+    public interface IProductRepository
+    {
+        // allow a caller to obtain a sequence of Product objects
+        IQueryable<Product> Products { get; }
+    }
+}
---
+++ Projekt/OnlineStore/OnlineStore/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineStore.Models;
+
+namespace OnlineStore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // when a component (controller) needs an implementation 
+            // of the IProductRepository interface, it should receive
+            // an instance of the TemporaryProductRepository class
+            // (a new FakeProductRepository object should be created 
+            // each time the IProductRepository interface is needed)
+            services.AddTransient<IProductRepository, TemporaryProductRepository>();
+            // set up shared objects
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // set up an HTTP request processor
+
+            // display a page that shows detailed information about exceptions
+            // **Enable the developer exception page only when the app is running in the Development environment.**
+            app.UseDeveloperExceptionPage();
+            // provide status code pages such as 404 Not Found
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/error-handling
+            app.UseStatusCodePages();
+            // enable support for serving static content from the wwwroot folder
+            app.UseStaticFiles();
+            // enable ASP.NET Core MVC
+            app.UseMvc(routes => {});
+        }
+    }
+}
