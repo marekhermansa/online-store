4ef23dd1d560fad78de42ddcef725768b92e30ac
Marek Hermansa <marekhermansa@gmail.com>
2018-03-28 23:23:24 +0200

    Add list of categories

---
+++ Projekt/OnlineStore/OnlineStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Models;
+
+namespace OnlineStore.Components {
+    public class NavigationMenuViewComponent : ViewComponent {
+        private IProductRepository repository;
+        public NavigationMenuViewComponent(IProductRepository repo) {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke() => 
+            View(repository.Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x));
+
+        //public string Invoke() => "the navigation view component";
+    }
+}
