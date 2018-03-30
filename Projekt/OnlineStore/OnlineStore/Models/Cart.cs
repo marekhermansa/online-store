cc9f9cc28539eb3b5ce92f5ec3502c9cfc162cf5
Marek Hermansa <marekhermansa@gmail.com>
2018-03-30 14:57:08 +0200

    Add add-to-cart button

--- Projekt/OnlineStore/OnlineStore/Models/Cart.cs
+++ Projekt/OnlineStore/OnlineStore/Models/Cart.cs
@@ -5,7 +5,44 @@ using System.Threading.Tasks;
 
 namespace OnlineStore.Models
 {
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+
     public class Cart
     {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection
+            .Where(p => p.Product.ProductID == product.ProductID)
+            .FirstOrDefault();
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+        public void RemoveLine(Product product) =>
+        lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+
+        public decimal ComputeTotalValue() =>
+        lineCollection.Sum(e => e.Product.Price * e.Quantity);
+
+        public void Clear() => lineCollection.Clear();
+
+        // get access to the contents of the cart
+        public IEnumerable<CartLine> Lines => lineCollection;
     }
 }
