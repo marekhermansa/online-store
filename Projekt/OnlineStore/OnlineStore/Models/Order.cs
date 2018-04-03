03c215bcba5f05953837083661dcf4235be8491e
Marek Hermansa <marekhermansa@gmail.com>
2018-04-03 17:55:10 +0200

    Restore delivery details panel

--- Projekt/OnlineStore/OnlineStore/Models/Order.cs
+++ Projekt/OnlineStore/OnlineStore/Models/Order.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace OnlineStore.Models
 {
     public class Order
     {
+        [BindNever]
+        public int OrderID { get; set; }
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+        [Required(ErrorMessage = "Enter a name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Enter the first address line")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        [Required(ErrorMessage = "Enter a city name")]
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public bool ShippingInsurance { get; set; }
     }
 }
