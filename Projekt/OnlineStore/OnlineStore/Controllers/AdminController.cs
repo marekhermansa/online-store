72c5ca2f3e71bf8066badc565b78712240affbd9
Marek Hermansa <marekhermansa@gmail.com>
2018-04-14 19:16:39 +0200

    add edit method for admin panel

--- Projekt/OnlineStore/OnlineStore/Controllers/AdminController.cs
+++ Projekt/OnlineStore/OnlineStore/Controllers/AdminController.cs
@@ -14,5 +14,9 @@ namespace SportsStore.Controllers
             repository = repo;
         }
         public ViewResult Index() => View(repository.Products);
+
+        public ViewResult Edit(int productId) =>
+            View(repository.Products
+                .FirstOrDefault(p => p.ProductID == productId));
     }
 }
