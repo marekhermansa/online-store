ff1ee959cf2e4ac08326f111ac5f04a019501873
Marek Hermansa <marekhermansa@gmail.com>
2018-04-14 18:23:28 +0200

    make amends to migrations

--- Projekt/OnlineStore/OnlineStore/Controllers/OrderController.cs
+++ Projekt/OnlineStore/OnlineStore/Controllers/OrderController.cs
@@ -1,31 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using OnlineStore.Models;
-
-namespace SportsStore.Controllers
+using System.Linq;
+namespace OnlineStore.Controllers
 {
     public class OrderController : Controller
     {
         private IOrderRepository repository;
         private Cart cart;
-
         public OrderController(IOrderRepository repoService, Cart cartService)
         {
             repository = repoService;
             cart = cartService;
         }
-
         public ViewResult Checkout() => View(new Order());
-
         [HttpPost]
         public IActionResult Checkout(Order order)
         {
             if (cart.Lines.Count() == 0)
             {
-                ModelState.AddModelError("", "Sorry, your cart is empty!");
+                ModelState.AddModelError("", "Your cart is empty!");
             }
 
             if (ModelState.IsValid)
