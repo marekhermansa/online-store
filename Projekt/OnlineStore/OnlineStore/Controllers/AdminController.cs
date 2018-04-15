7508c301e10757253b7dd581ef119b195b37de05
Marek Hermansa <marekhermansa@gmail.com>
2018-04-15 11:18:48 +0200

    add account controller

--- Projekt/OnlineStore/OnlineStore/Controllers/AdminController.cs
+++ Projekt/OnlineStore/OnlineStore/Controllers/AdminController.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OnlineStore.Models;
+using Microsoft.AspNetCore.Authorization;
+
 namespace SportsStore.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         private IProductRepository repository;
